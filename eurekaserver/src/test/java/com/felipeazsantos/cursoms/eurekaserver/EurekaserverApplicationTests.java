45a6a1992f74ca449f102f5fb1eaf0643917c8c6
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-03-14 17:57:33 -0300

    Criação e Configuração do Discovery Server Eureka

---
+++ eurekaserver/src/test/java/com/felipeazsantos/cursoms/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipeazsantos.cursoms.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
