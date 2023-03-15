566f21e4f012319b2629a204dc81126ddb83c530
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-03-15 14:40:32 -0300

    Testando Loadgin balance

--- msclientes/src/main/java/com/felipeazsantos/cursoms/msclientes/application/ClientesResource.java
+++ msclientes/src/main/java/com/felipeazsantos/cursoms/msclientes/application/ClientesResource.java
@@ -3,6 +3,7 @@ package com.felipeazsantos.cursoms.msclientes.application;
 import com.felipeazsantos.cursoms.msclientes.application.representation.ClienteSaveRequest;
 import com.felipeazsantos.cursoms.msclientes.domain.Cliente;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,12 +14,14 @@ import java.util.Optional;
 @RestController
 @RequestMapping("clientes")
 @RequiredArgsConstructor
+@Slf4j
 public class ClientesResource {
 
     private final ClienteService service;
 
     @GetMapping
     public String status() {
+        log.info("Obtendo o status do microservice de clientes");
         return "ok";
     }
 
