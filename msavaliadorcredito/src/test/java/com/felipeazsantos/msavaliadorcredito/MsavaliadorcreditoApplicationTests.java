d242335de3e9e4506d07726198758d52a580b0b8
Felipe Azevedo dos Santos <felipe.azevedo@cws.digital>
2023-03-16 16:00:51 -0300

    Criando e configurando o avaliador de crédito

---
+++ msavaliadorcredito/src/test/java/com/felipeazsantos/msavaliadorcredito/MsavaliadorcreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipeazsantos.msavaliadorcredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavaliadorcreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
